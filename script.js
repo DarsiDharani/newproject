19c9b76f157147307da53c5347dbd15dbb2e38f7
LearnWithDharani <149897190+LearnWithDharani@users.noreply.github.com>
2023-11-04 23:17:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let displayValue = '';
+
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = displayValue;
+}
+
+function calculateResult() {
+    try {
+        const result = eval(displayValue);
+        displayValue = result;
+        document.getElementById('display').value = result;
+    } catch (error) {
+        displayValue = 'Error';
+        document.getElementById('display').value = displayValue;
+    }
+}
+
